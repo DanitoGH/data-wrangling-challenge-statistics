7276501d34739cf11ce0f310ade88d28e694435f
Daniel Adjei Boateng <freshdan24@gmail.com>
2021-08-03 08:45:20 +0000

    React router dom library installed

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,45 @@
-import { Navigate, useRoutes } from 'react-router-dom';
-// layouts
-import DashboardLayout from './layouts/dashboard';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-//
 import DashboardApp from './pages/DashboardApp';
+// material
+import { styled } from '@material-ui/core/styles';
 
 // ----------------------------------------------------------------------
 
-export default function Router() {
-  return useRoutes([
-    {
-      path: '/dashboard',
-      element: <DashboardLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/dashboard/app" replace /> },
-        { path: 'app', element: <DashboardApp /> },
-      ]
-    },
-    {
-      path: '/',
-      children: [
-        { path: '/', element: <Navigate to="/dashboard" /> },
-        { path: '*', element: <Navigate to="/404" /> }
-      ]
-    },
-  ]);
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 40;
+
+const RootStyle = styled('div')({
+  display: 'flex',
+  minHeight: '100%',
+  overflow: 'hidden'
+});
+
+const MainStyle = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  overflow: 'auto',
+  minHeight: '100%',
+  paddingTop: APP_BAR_MOBILE + 24,
+  paddingBottom: theme.spacing(10),
+  [theme.breakpoints.up('lg')]: {
+    paddingTop: APP_BAR_DESKTOP + 24,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
+  }
+}));
+
+export default function Routes() {
+  return (
+    <Router>
+     <RootStyle>
+      <MainStyle>
+      <Switch>
+      <Route path="/">
+          <DashboardApp />
+       </Route>
+     </Switch>
+     </MainStyle>
+    </RootStyle>
+   </Router>
+  )
 }
